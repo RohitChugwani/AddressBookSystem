ac96517eb90b85256e840197a6e50ccdc7779eec
RohitChugwani <rchugwani3@gmail.com>
2022-06-10 14:41:13 +0530

    Created object for Uc1

--- src/Welcome.java
+++ src/Welcome.java
@@ -2,5 +2,8 @@ public class Welcome {
     public static void main(String[] args)
     {
         System.out.println("Welcome to address book System");
+        //creating object for UC1
+        UC1 rohit = new UC1("Rohit","raj","samadhiya colony","gwalior","MP","xyz@gmail.com",474001,"9516654766");
+        rohit.show_details();
     }
 }
