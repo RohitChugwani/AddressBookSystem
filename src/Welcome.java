7f7efee6a25e64d85abf6789e3f52bc0328658d4
RohitChugwani <rchugwani3@gmail.com>
2022-06-10 15:06:38 +0530

    craeted object for UC2 to take contact details from console and displaying it

--- src/Welcome.java
+++ src/Welcome.java
@@ -5,5 +5,9 @@ public class Welcome {
         //creating object for UC1
         UC1 rohit = new UC1("Rohit","raj","samadhiya colony","gwalior","MP","xyz@gmail.com",474001,"9516654766");
         rohit.show_details();
+        //creating object for UC2 ,adding info from console,displaying the details
+        UC2 mohit = new UC2();
+        mohit.add_details();
+        mohit.show_details();
     }
 }
